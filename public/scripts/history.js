fa90ec100f3e9adcf628cbfcfb4b57d197308fbb
Mike Kissinger <mis4mike@gmail.com>
2013-09-28 00:32:26 -0400

    more fiddling with the history page

--- public/scripts/history.js
+++ public/scripts/history.js
@@ -1,11 +1,20 @@
 $(document).on('ready', function () {
   $('.era-icon').hover( function () {
     $(this).parents('.era-institution').addClass('active');
+    $(this).parents('.era').find('.institution-name').html($(this).parent().data('name'));
   }, function () {
     $(this).parents('.era-institution').removeClass('active');    
+    $(this).parents('.era').find('.institution-name').html('');
   });
 
   $('.era-icon').on('click', function () {
-    $('#lightbox').css('height', '100%');
+    $('#lightbox').addClass('active');
+    $('#lightbox-title').html($(this).parent().data('name'));
+
   });
+
+  $('#lightbox .close').on('click', function () {
+    $('#lightbox').removeClass('active');
+
+  });  
 });
